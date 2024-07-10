373ce52c224430dbe37de0a85743ee6ebdaeffc5
Indry Sefviana <indrysfa@gmail.com>
2024-07-10 11:25:39 +0700

    add auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role');
+            $table->integer('status');
             $table->rememberToken();
             $table->timestamps();
         });
