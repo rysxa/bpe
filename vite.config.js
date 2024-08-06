0edb5efd2a678f41eea062a3790415ca00fd712b
Indry Sefviana <indrysfa@gmail.com>
2024-08-06 12:16:35 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,49 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import vue from '@vitejs/plugin-vue';
+// import path from 'path';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/sass/app.scss',
+//                 'resources/js/app.js',
+//                 'resources/vendors/js/vendor.bundle.base.js'
+//             ],
+//             refresh: true,
+//         }),
+//         vue({
+//             template: {
+//                 transformAssetUrls: {
+//                     base: null,
+//                     includeAbsolute: false,
+//                 },
+//             },
+//         }),
+//     ],
+//     server: {
+//         host: '0.0.0.0',
+//         hmr: {
+//             host: '127.0.0.1',
+//         },
+//     },
+//     resolve: {
+//         alias: {
+//             vue: 'vue/dist/vue.esm-bundler.js',
+//             '$': 'jquery',
+//             'jQuery': 'jquery',
+//             'window.jQuery': 'jquery',
+//         },
+//     },
+//     optimizeDeps: {
+//         include: ['jquery'],
+//         exclude: ['jquery.cookie'],
+//     },
+// });
+
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
@@ -11,33 +53,10 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/vendors/js/vendor.bundle.base.js'
             ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
+            refresh: [
+                ...refreshPaths,
+                'app/Livewire/**',
+            ],
         }),
     ],
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: '127.0.0.1',
-        },
-    },
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-            '$': 'jquery',
-            'jQuery': 'jquery',
-            'window.jQuery': 'jquery',
-        },
-    },
-    optimizeDeps: {
-        include: ['jquery'],
-        exclude: ['jquery.cookie'],
-    },
 });
